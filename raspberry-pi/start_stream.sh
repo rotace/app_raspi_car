515a0b28ce02c25e7c415e7337a4f1a3b92158c7
YS.VostroPC <rotace2012227ys@gmail.com>
2018-01-05 11:57:27 +0900

    adjust start_stream.sh

--- raspberry-pi/start_stream.sh
+++ raspberry-pi/start_stream.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+DIR="/home/pi/workspace/mjpg-streamer/mjpg-streamer-experimental/"
 
 if pgrep mjpg_streamer > /dev/null
 then
 echo "mjpg_streamer already running"
 else
-LD_LIBRARY_PATH=/opt/mjpg-streamer/ /opt/mjpg-streamer/mjpg_streamer -i "input_raspicam.so -fps 15 -q 50 -x 640 -y 480" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www" > /dev/null 2>&1&
-echo "mjpg_streamer started"
+LD_LIBRARY_PATH=$DIR $DIR/mjpg_streamer -i "input_raspicam.so -fps 10 -q 20 -x 640 -y 480" -o "output_http.so -p 9000 -w /opt/mjpg-streamer/www"
 fi
